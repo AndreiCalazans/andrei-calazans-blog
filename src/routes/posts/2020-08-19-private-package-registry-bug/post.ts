ca5de4245f3f1c1dd1bcfdf52fcd61c502b3d94c
Andrei Calazans <andreixoc@hotmail.com>
2020-08-19 15:05:31 -0300

    post: private package registry bug

---
+++ src/routes/posts/2020-08-19-private-package-registry-bug/post.ts
@@ -0,0 +1,7 @@
+export default {
+  title: "NPM & Yarn Private Package Registry Bug",
+  tags: ['npm', 'yarn', 'private registry'],
+  spoiler: 'While working on a client project, I ran into an issue with their private registry. I kept seeing the unauthorized error when I ran `yarn install`. It was a weird issue especially because I had all of the correct keys. After some investigation I found out I was using the wrong `_auth` value.',
+  getContent: () => import('./document.mdx'),
+}
+
