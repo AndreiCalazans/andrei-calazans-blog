9761baba527792162742577c75465373a04dc0fa
Andrei Calazans <andreixoc@hotmail.com>
2020-10-24 15:28:22 -0300

    How to avoid learned helplessness

---
+++ src/routes/posts/2020-10-24-how-avoid-learned-helplessness/post.tsx
@@ -0,0 +1,7 @@
+export default {
+  title: "How To Avoid Learned Helplessness Syndrome?",
+  tags: ['career', 'Learned Helplessness'],
+  spoiler: 'Are you always asking for help? Does the task at hand feel completely overwhelming and you don\'t know where to begin or what to do? You could be suffering of \"learned helplessness\".',
+  getContent: () => import('./document.mdx'),
+}
+
