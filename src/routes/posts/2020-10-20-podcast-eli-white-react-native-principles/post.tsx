3d301de832c0ed6cbfb5e5372c157fb0acab0d3f
Andrei Calazans <andreixoc@hotmail.com>
2020-10-31 13:55:11 -0300

    Add Eli Whites podcast interview summary

---
+++ src/routes/posts/2020-10-20-podcast-eli-white-react-native-principles/post.tsx
@@ -0,0 +1,7 @@
+export default {
+  title: "Podcast Summary - Eli White on the princiciples of React Native and more.",
+  tags: ['react-native', 'podcast'],
+  spoiler: "I pinpoint the parts I found interesting in this podcast where Eli White revists React Native's principles and more.",
+  getContent: () => import('./document.mdx'),
+}
+
