461afcbd4db39bf1feccc77c4339e19499d0e8f9
Andrei Calazans <andreixoc@hotmail.com>
2020-11-01 16:11:41 -0300

    Add how to create a native view module with you.i

---
+++ src/routes/posts/2020-10-25-how-to-create-a-native-view-with-react-native-youi/post.tsx
@@ -0,0 +1,7 @@
+export default {
+  title: "How to create a native view module with react-native-youi",
+  tags: ['react-native', 'You.i', 'how-to'],
+  spoiler: "This post will walk you through the steps to create a Native View Module for react-native-youi library.",
+  getContent: () => import('./document.mdx'),
+}
+
