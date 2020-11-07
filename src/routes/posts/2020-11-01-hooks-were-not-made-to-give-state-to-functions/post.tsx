51f2a2fec841e74b61ab87d6fa09d056be785fd5
Andrei Calazans <andreixoc@hotmail.com>
2020-11-07 19:48:29 -0300

    Add hooks were not made to give state to functions

---
+++ src/routes/posts/2020-11-01-hooks-were-not-made-to-give-state-to-functions/post.tsx
@@ -0,0 +1,7 @@
+export default {
+  title: "Hooks Were Not Made To Give State To Functions",
+  tags: ['hooks', 'react'],
+  spoiler: "There is a misconception that hooks were created to simply give state to functions. This post goes over the reasons hooks were created and why you should know them.",
+  getContent: () => import('./document.mdx'),
+}
+
