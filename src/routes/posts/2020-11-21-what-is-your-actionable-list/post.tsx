d5d47009079722398edbffa7b08edc84292b8f83
Andrei Calazans <andreixoc@hotmail.com>
2020-11-21 10:57:34 -0300

    list the 100 actions to do

---
+++ src/routes/posts/2020-11-21-what-is-your-actionable-list/post.tsx
@@ -0,0 +1,7 @@
+export default {
+  title: "What are the 100 things you must do accomplish what you want?",
+  tags: ['career'],
+  spoiler: "This came up while I was thinking about my goals. To become a developer I came up with a plan that resulted in a simple list of actions I had to accomplish to achieve what I wanted.",
+  getContent: () => import('./document.mdx'),
+}
+
