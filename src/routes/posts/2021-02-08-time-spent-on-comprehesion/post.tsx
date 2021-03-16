9a6c9b9ac4fc312bfb0cdbccaffcec99d81aa9d6
Andrei Calazans <andreixoc@hotmail.com>
2021-03-16 08:42:16 -0300

    Time Spent On Comprehension

---
+++ src/routes/posts/2021-02-08-time-spent-on-comprehesion/post.tsx
@@ -0,0 +1,7 @@
+export default {
+  title: "Code Comprehension Takes The Largerst Amount Of Time",
+  tags: ['programming'],
+  spoiler: "Code comprehension represents the largest amount of time spent with a codebase",
+  getContent: () => import('./document.mdx'),
+}
+
