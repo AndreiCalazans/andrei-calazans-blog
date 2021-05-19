522ea3cbf721a19a210ece8fdb31071bc280f218
Andrei Calazans <andreixoc@hotmail.com>
2021-05-19 07:19:08 -0300

    Questions To Engineering

---
+++ src/routes/posts/2021-05-19-engineering-dep-questions-to-answer/post.ts
@@ -0,0 +1,7 @@
+export default {
+  title: "Questions To Ask Your Engineering",
+  tags: ['engineering'],
+  spoiler: 'What are some questions every engineering department should try to answer?',
+  getContent: () => import('./document.mdx'),
+}
+
