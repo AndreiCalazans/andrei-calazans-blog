3e911b3c9cfb96e57ff742b8712321bc045647a7
Andrei Calazans <andreixoc@hotmail.com>
2021-05-27 05:57:32 -0300

    Test driven learning

---
+++ src/routes/posts/2021-05-27-test-driven-learning/post.ts
@@ -0,0 +1,8 @@
+export default {
+  title: "Test Driven Learning",
+  tags: ['career', 'engineering'],
+  spoiler: 'When programming it is ideal to always optimize for faster iterations, but what about learning?',
+  getContent: () => import('./document.mdx'),
+}
+
+
