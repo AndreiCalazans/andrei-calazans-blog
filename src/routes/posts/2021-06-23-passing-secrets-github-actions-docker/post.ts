0f166662863112dcab317d58fb4afafd8ce1a6b8
Andrei Calazans <andreixoc@hotmail.com>
2021-06-23 15:38:19 -0300

    Github Action Secrets & Dockerfiles

---
+++ src/routes/posts/2021-06-23-passing-secrets-github-actions-docker/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: "Sharing environment variables using Github Action secrets",
+  tags: ['devops', 'engineering'],
+  spoiler: 'I spent a few hours trying to figure out how to consume a Github secret within a Dockerfile, this post shows you how to do it.',
+  getContent: () => import('./document.mdx'),
+}
