c00fc9d69a0faef1abbd41a06aff76df07e3743c
Andrei Calazans <andreixoc@hotmail.com>
2021-10-22 10:47:10 -0300

    fix duplicated post tags

--- src/routes/posts/2020-06-03-tv-apps-handling-focus-styling-in-cpp-for-performance-gains-with-react-native-youi/post.ts
+++ src/routes/posts/2020-06-03-tv-apps-handling-focus-styling-in-cpp-for-performance-gains-with-react-native-youi/post.ts
@@ -1,6 +1,6 @@
 export default {
   title: `TV Apps — Handling Focus Styling In C++ For Performance Gains With React Native You.i`,
-  tags: ['react', 'react native', 'You.i'],
+  tags: ['react', 'react-native', 'You.i'],
   spoiler: 'When specifying the behavior of a data structure component, there are often two concerns that need to be dealt with: basic functionality and support for data integrity. A data structure component is often easier to use if these two concerns are divided as much as possible into separate sets of client functions.²',
   getContent: () => import('./document.mdx'),
 }
