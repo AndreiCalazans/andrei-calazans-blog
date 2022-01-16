459283eea2589bd8155f783ecc4db77169136e34
Andrei Calazans <andreixoc@hotmail.com>
2022-01-16 15:24:34 -0300

    Write react native weekly week 3 2022

---
+++ src/routes/posts/2022-01-17-react-native-weekly/post.ts
@@ -0,0 +1,7 @@
+export default {
+  title: "React Native Weekly - W3 2022",
+  tags: ["react-native"],
+  spoiler: "First of 2022 React Native Weekly. Covers updates since ...",
+  getContent: () => import("./document.mdx"),
+}
+
