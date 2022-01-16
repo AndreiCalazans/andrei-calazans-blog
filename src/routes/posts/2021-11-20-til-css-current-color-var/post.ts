ce201850dbc535350a77e04c975fe39b0335f04a
Andrei Calazans <andreixoc@hotmail.com>
2022-01-16 16:14:39 -0300

    Write first TIL

---
+++ src/routes/posts/2021-11-20-til-css-current-color-var/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: "TIL - currentColor keyword in CSS",
+  tags: ["TIL", "CSS"],
+  spoiler: "Today I learned what the currentColor keyword does in CSS",
+  getContent: () => import("./document.mdx"),
+};
