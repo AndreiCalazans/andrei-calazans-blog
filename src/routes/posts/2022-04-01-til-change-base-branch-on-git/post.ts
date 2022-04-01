b3b803cfb15a89899d819fa9e644fd4926fb71dc
Andrei Calazans <andreixoc@hotmail.com>
2022-04-01 08:42:07 -0300

    Write TIL til-change-base-branch-on-git

---
+++ src/routes/posts/2022-04-01-til-change-base-branch-on-git/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: "How do you change the base of a branch in Git?",
+  tags: ["TIL", "git"],
+  spoiler: "Today I learned git rebase --onto",
+  getContent: () => import("./document.mdx"),
+};
