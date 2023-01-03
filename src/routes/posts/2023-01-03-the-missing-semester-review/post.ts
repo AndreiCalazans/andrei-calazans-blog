8de3ca53ae483ad14456441148c1bb9c90de9202
Andrei Calazans <andrei.calazans@coinbase.com>
2023-01-03 10:50:37 -0300

    The missing semester

---
+++ src/routes/posts/2023-01-03-the-missing-semester-review/post.ts
@@ -0,0 +1,7 @@
+export default {
+  title: "Why you should do “The Missing Semester” by MIT",
+  tags: ["engineering", "vim", "terminal"],
+  spoiler:
+    "Have you heard of “The Missing Semester” course provided by a team of instructors at MIT? They promise to go over a set of tools which can help you become a more proficient software engineer.",
+  getContent: () => import("./document.mdx"),
+};
