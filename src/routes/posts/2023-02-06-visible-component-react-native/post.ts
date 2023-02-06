5352931a28c60302a44ce0d88a431253e4338efe
Andrei Calazans <andreixoc@hotmail.com>
2023-02-06 17:05:03 -0300

    Daily 02-06-2023

---
+++ src/routes/posts/2023-02-06-visible-component-react-native/post.ts
@@ -0,0 +1,8 @@
+export default {
+  title:
+    "How do you toggle a component's visibility without losing its UI space?",
+  tags: ["react-native", "react"],
+  spoiler:
+    "Simple implementation of a Visible component that enables you to toggle the visibility of any component.",
+  getContent: () => import("./document.mdx"),
+};
