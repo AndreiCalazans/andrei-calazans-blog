1d3c30af9197079194f411540cc77d01e8b58357
Andrei Calazans <andreixoc@hotmail.com>
2023-11-16 11:07:58 -0300

    Update tags

--- src/routes/posts/2023-11-16-react-native-performance-book-course/post.ts
+++ src/routes/posts/2023-11-16-react-native-performance-book-course/post.ts
@@ -1,7 +1,7 @@
 export default {
   title:
     "I'm Writing a Book/Course On React Native Performance Tooling",
-  tags: ["work"],
+  tags: ["react-native", "performance", "book", "course"],
   spoiler:
     "I'm writing a book and course about the tooling one can use to idenfity performance bottlenecks on React Native.",
   getContent: () => import("./document.mdx"),
