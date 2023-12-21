b385b43e778763f398e1eb5e7c29bdf3367f50e0
Andrei Calazans <andreixoc@hotmail.com>
2023-12-20 22:36:10 -0300

    Start migrating info

--- thoughtfulLounge/src/site.config.ts
+++ thoughtfulLounge/src/site.config.ts
@@ -2,18 +2,18 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Andrei Calazans",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Andrei Calazans",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "A lounge where you find thoughts on software development, productivity, and life.",
 	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-GB",
+	lang: "en-US",
 	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_GB",
+	ogLocale: "en_US",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
-		locale: "en-GB",
+		locale: "en-US",
 		options: {
 			day: "numeric",
 			month: "short",
@@ -21,7 +21,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	webmentions: {
-		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+		link: "https://webmention.io/andrei-calazans.com/webmention",
 	},
 };
 
@@ -38,5 +38,13 @@ export const menuLinks: Array<{ title: string; path: string }> = [
 	{
 		title: "Blog",
 		path: "/posts/",
+	},
+	{
+		title: "Tags",
+		path: "/tags",
+	},
+	{
+		title: "RSS",
+		path: "/rss",
 	},
 ];
