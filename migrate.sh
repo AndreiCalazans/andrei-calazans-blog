388f2c70a256f68150342bcc32241650aec90a8b
Andrei Calazans <andreixoc@hotmail.com>
2023-12-21 12:23:16 -0300

    Making progress

--- migrate.sh
+++ migrate.sh
@@ -3,19 +3,28 @@ set -eo pipefail
 
 echo 'hello'
 
-routes=$(fd '.*weekly')
+# routes=$(fd '.*weekly')
 
 # Step one: Take the weekly posts and move them to thoughtfulLounge/src/pages
-for route in $routes; do
-  folder=$(basename $route)
-  mv $route thoughtfulLounge/src/pages/$folder
-done
+# for route in $routes; do
+#   folder=$(basename $route)
+#   mv $route thoughtfulLounge/src/pages/$folder
+# done
 
 
-# Step two: Take the exported object and convert to frontmatter
-for page in thoughtfulLounge/src/pages/*weekly; do
-  date=$(basename $page | cut -d- -f1)
-  object=$(cat $page/post.ts | grep -E 'title|tags|spoiler' | sed 's/export default {//g' | sed 's/};//g')
-  frontmatter="---\ntitle: $(echo "$object" | grep title | cut -d: -f2 | sed 's/"//g')\npublishDate: $date\n$(echo "$object" | grep tags | sed 's/tags://g')\n---"
-  echo "$frontmatter" > $page/index.md
+# # Step two: Take the exported object and convert to frontmatter
+for page in thoughtfulLounge/src/content/post/*weekly; do
+  date=$(basename $page | cut -c 1-10)
+  echo $(fd -t f 'post.*' $page)
+  object=$(fd -t f 'post.*' $page | xargs cat)
+  echo "what $object"
+  title=$(echo "$object" | grep title | cut -d: -f2 | sed 's/"]//g' | sed 's/,$//g')
+  tags=$(echo "$object" | grep tags | sed 's/tags://g' | sed 's/,$//g')
+  frontmatter="---\ntitle: $title\npublishDate: "$date"\ntags: $tags\n---"
+
+  sed -i '' '1s/^/$frontmatter\n/' $page/index.mdx
+
+  # echo "$frontmatter"  > $tmpfile
+  # cat $page/index.md >> $tmpfile
+  # mv $tmpfile $page/index.md
 done
