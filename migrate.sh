baa68e092df03c78dd36c4d3ebda9686487fced2
Andrei Calazans <andreixoc@hotmail.com>
2023-12-21 11:31:03 -0300

    First verfsion

--- migrate.sh
+++ migrate.sh
@@ -3,51 +3,19 @@ set -eo pipefail
 
 echo 'hello'
 
-
-# Steps 
-#
-# Step one:
-# Take the weekly posts and move them to 
-# thoughtfulLounge/src/pages
-#
-# Step two:
-# Take the exported object from post.ts inside
-# the weekly folder and move it to the md or mdx file in the format of
-# Frontmatter
-#
-# example:
-# The object in post.ts
-#
-# export default {
-#   title: "React Native Weekly - W35 2021",
-#   tags: ["react-native", "react-native-weekly"],
-#   spoiler: "",
-#   getContent: () => import("./document.mdx"),
-# };
-#
-# will be turned into:
-#
-# title: "React Native Weekly - W35 2021"
-# publishDate: "30 January 2023"
-# description: ""
-# tags: ["react-native", "react-native-weekly"]
-# 
-# attention, the publishDate will be retrieved from the folder name
-# example:
-# When folder is src/routes/posts/2022-01-17-react-native-weekly/ 
-# the publishDate is 17 January 2022
-#
-
 routes=$(fd '.*weekly')
 
-echo $routes
-# Example of routes
-# src/routes/posts/2022-01-17-react-native-weekly/
-# src/routes/posts/2022-01-24-react-native-weekly/
-# src/routes/posts/2022-01-30-react-native-weekly/
-# src/routes/posts/2022-02-07-react-native-weekly/
-# src/routes/posts/2022-02-27-react-native-weekly/
-# src/routes/posts/2022-03-06-react-native-weekly/
-# src/routes/posts/2022-03-22-react-native-weekly/
+# Step one: Take the weekly posts and move them to thoughtfulLounge/src/pages
+for route in $routes; do
+  folder=$(basename $route)
+  mv $route thoughtfulLounge/src/pages/$folder
+done
 
-# thoughtfulLounge/src/pages/posts/[slug].astro
+
+# Step two: Take the exported object and convert to frontmatter
+for page in thoughtfulLounge/src/pages/*weekly; do
+  date=$(basename $page | cut -d- -f1)
+  object=$(cat $page/post.ts | grep -E 'title|tags|spoiler' | sed 's/export default {//g' | sed 's/};//g')
+  frontmatter="---\ntitle: $(echo "$object" | grep title | cut -d: -f2 | sed 's/"//g')\npublishDate: $date\n$(echo "$object" | grep tags | sed 's/tags://g')\n---"
+  echo "$frontmatter" > $page/index.md
+done
